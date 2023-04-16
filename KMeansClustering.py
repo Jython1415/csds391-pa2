8766ecc1a1efe8e71c9734d6fa288184c9b9fa48
Joshua Shew <joshua.t.shew@gmail.com>
2023-04-16 15:37:50 -0400

    added plotting and fixed updating

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -1,6 +1,9 @@
 import math
 import random
 from statistics import mean
+from IrisData import IrisData
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 
 class DataEntry:
     
@@ -69,11 +72,15 @@ class KMeansClustering:
     data:Dataset = None         # Stores the data (points and labels)
     clusters:list[Cluster] = [] # Stores the k clusters
     
+    # Output data
+    objFunc = []
+    clusterMaps = []
+    
     # rawData must be an array with float parameters and one label entry (str or int)
-    def __init__(self, rawData, k = 3, maxItr = 1000):
+    def __init__(self, rawData:IrisData, k = 3, maxItr = 1000):
         self.k = k
         self.maxItr = maxItr
-        self.rawData = rawData.copy
+        self.rawData = rawData.getData().copy()
         
     def prepareData(self):
         
@@ -83,10 +90,10 @@ class KMeansClustering:
         
         for index, entry in enumerate(self.rawData[0]):
             if type(entry) == float:
-                self.paramIndices.append(index)
+                paramIndices.append(index)
             elif type(entry) == str or type(entry) == int:
                 if labelIndex == -1:
-                    self.labelIndex = index
+                    labelIndex = index
                 else:
                     raise Exception(f"rawData has more than one label: {labelIndex} and {index}")
         
@@ -107,6 +114,7 @@ class KMeansClustering:
     
     def initialize(self):
         self.prepareData()
+        self.clusters.clear()
         
         # Create k random clusters
         for _ in range(self.k):
@@ -118,18 +126,46 @@ class KMeansClustering:
     
     def iterate(self):
         # Assign points to clusters
-        map(Cluster.clearPoints(), self.clusters)
+        for cluster in self.clusters:
+            cluster.clearPoints()
+        
         for point in self.data.getPoints():
             distances = [math.dist(cluster.getCentroid(), point) ** 2 for cluster in self.clusters]
             self.clusters[distances.index(min(distances))].addPoint(point)
         
         # Find new centroid for each cluster
-        map(Cluster.findCentroid(), self.clusters)
+        for cluster in self.clusters:
+            cluster.findCentroid()
     
     def run(self):
         self.initialize()
         
-        for iter in range(self.maxItr):
+        for _ in range(self.maxItr):
             self.iterate()
+            
+            # Record data
+            self.objFunc.append(self.objectiveFunction())
+            # self.clusterMaps.append(self.plotClusters())
+            
+            # Check end condition
+            if len(self.objFunc) >= 2 and self.objFunc[-1] == self.objFunc[-2]:
+                break
         
+    def plotClusters(self):
+        # Get the colors
+        colors = sorted(mcolors.TABLEAU_COLORS, key=lambda c: tuple(mcolors.rgb_to_hsv(mcolors.to_rgb(c))))
+        colors = list(reversed(colors))
+        colors = [colors[i*3 % len(colors)] for i in range(len(colors))]
+        colors = colors[:len(self.clusters)]
         
+        # Plot the values
+        fig, ax = plt.subplots()
+        for i, cluster in enumerate(self.clusters):
+            ax.scatter([point[2] for point in cluster.getPoints()], [point[3] for point in cluster.getPoints()], s=10, color=colors[i])
+            ax.scatter(cluster.getCentroid()[2], cluster.getCentroid()[3], s=30, color='r', alpha=.5)
+        
+        ax.set_xlabel('Sepal Length')
+        ax.set_ylabel('Petal Length')
+        ax.set_title('Petal Length vs. Sepal Length with Colored Clusters')
+        
+        return fig
